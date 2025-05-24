f3036da9fb8da53a875a34dda04afaf360b95c11
榊原昌彦 <sakakibara@rdlabo.jp>
2025-05-24 22:56:03 +0900

    chore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./main/page/main.component').then((m) => m.MainComponent),
   },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
