972a8e15c0f93ce46da8670c19ab109cd168b6bf
lebenaton <70837142+lebenaton@users.noreply.github.com>
2026-05-25 15:34:48 +0900

    Update announcement.ts

--- src/data/announcement.ts
+++ src/data/announcement.ts
@@ -3,7 +3,7 @@ import { IItem } from '../config/types';
 export const announcement: IItem[] = [
   {
     image: 'assets/images/announcement/1738021800759.png',
-    title: 'le panierホームページを更新しました。',
+    title: 'ル　パニエのホームページがリニューアル',
     body: '日替わりなどもホームページで確認できます。<a href="https://le-panier.net/?from=card">https://le-panier.net/?from=card</a><br>色々新しいことに挑戦しているお店です、一度ぜひ遊びに来てください。<br>ル　パニエといいます、お惣菜の販売、店内飲食カフェやワインバーとしてもお楽しみいただけます。<br>皆様のご来店をお待ちしております。',
   },
 ];
